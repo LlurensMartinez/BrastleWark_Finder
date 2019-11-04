25d3ca2dcf41c11e17761cd90dd9d06458b9641b
Llorenç <crack.one.7@gmail.com>
2019-11-04 12:30:32 +0100

    Show listCards Gnomes

--- src/components/GnomeCard.js
+++ src/components/GnomeCard.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 const GnomeCard = ({gnome}) => {
 
-  console.log(gnome)
-  
   return (
     <div>
-      
+      <img src={gnome.thumbnail} alt={gnome.name}/>
+      <p>Name: {gnome.name}</p>
+      <p>Age: {gnome.age}</p>
+      <p>Weight: {gnome.weight.toFixed(2)} Kg</p>
+      <a href="!#">Details</a>
     </div>
   );
 };
