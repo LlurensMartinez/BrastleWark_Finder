0abe8341e45e7da82f154dcdd2653d9989a0b17a
Llorenç <crack.one.7@gmail.com>
2019-11-06 16:37:58 +0100

    No results search

--- src/components/ListCards/ListCards.js
+++ src/components/ListCards/ListCards.js
@@ -5,16 +5,19 @@ import './listCards.css';
 const ListCards = ({gnomes, data}) => {
 
   const render = gnomes ? gnomes : data
-  
+
   return (
     <div className="cardsWrapped">
       {
+      render.length != 0 ?
       render.map(gnome =>(
         <GnomeCard 
             key={gnome.id}
             gnome={gnome}
         />
       ))
+      :
+      <p>No results for your search</p>
     }
     </div>
   );
